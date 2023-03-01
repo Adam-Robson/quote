3d0c266f5ae5c749ec59031441cb98ed50f471b5
adam robson <adamray312@gmail.com>
2023-02-28 17:22:43 -0800

    add files

---
+++ App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import { quotes } from './data.js';
+
+export default function App() {
+  const [index, setIndex] = useState(0);
+
+  const handleClick = () => {
+    const number = Math.floor(Math.random() * quotes.length);
+    setIndex(number);
+    quote = quotes[number];
+    return quote;
+  };
+  let quote = quotes[index];
+
+  return (
+    <>
+      <div id="home-wrapper">
+        <section className="quote-container">
+          <h3 id="quote-title">quote for the day</h3>
+          <div className="quote-box">
+            <h4 id="text">{ quote.text }</h4>
+            <div className="author-wrap">
+              <h4 id="author">-{ quote.author }</h4>
+            </div>
+            <div id="button-container">
+              <button id="next-quote" onClick={ handleClick }>
+                new quote
+              </button>
+              <a href="twitter.com/intent/tweet" target='_blank'>tweet</a>
+            </div>
+          </div>
+        </section>
+        <section className="image-container">
+        </section>
+      </div>
+    </>
+  );
+}
+
---
+++ data.js
@@ -0,0 +1,59 @@
+export const quotes = [
+  {
+    id: 1,
+    author: 'george carlin',
+    text: 'if you can\'t say something nice about someone, go ahead',
+    url: './images/carlin.png',
+    alt: 'image of george carlin',
+  },
+  {
+    id: 2,
+    author: 'carl sagan',
+    text: 'if you wish to make an apple pie from scratch, you must first invent the universe.',
+    url: './images/sagan.png',
+    alt: 'image of carl sagan.'
+  },
+  {
+    id: 3,
+    author: 'jim jarmusch',
+    text: 'nothing is original. steal from anywhere that resonates with inspiration or fuels your imagination. devour old films, new films, music, books, paintings, photographs, poems, dreams, random conversations, architecture, bridges, street signs, trees, clouds, bodies of water, light and shadows. select only things to steal from that speak directly to your soul. if you do this, your work (and theft) will be authentic. authenticity is invaluable; originality is nonexistent. and dont bother concealing your thievery, celebrate it if you feel like it.',
+    url: './images/jarmusch.png',
+    alt: 'image of director jim jarmusch.'
+  },
+  {
+    id: 4,
+    author: 'robert marley',
+    text: 'the stone that the builder refused, will always be the head-cornerstone',
+    url: './images/marley.png',
+    alt: 'image of robert marley'
+  },
+  {
+    id: 5,
+    author: 'charles bukowski',
+    text: 'Find what you love and let it kill you.',
+    url: './images/bukowski.png',
+    alt: 'image of charles bukowski'
+  },
+  {
+    id: 6,
+    author: 'muhammad ali',
+    text: 'impossible is not a fact. its an opinion. impossible is not a declaration. it is a dare. impossible is potential.impossible is temporary. impossible is nothing.',
+    url: './images/ali.png',
+    alt: 'image of muhammad ali'
+  },
+  {
+    id: 7,
+    author: 'jorge luis borges',
+    text: 'the original is unfaithful to the translation.',
+    url: './images/borges.png',
+    alt: 'image of author jorge borges.'
+  },
+  {
+    id: 8,
+    author: 'kurt vonnegut',
+    text: 'egregious.  most people think that word means terrible or unheard of or unforgivable.  it has a much more interesting story than that to tell.  it means outside the herd.  imagine that: thousands of people, outside the herd.',
+    url: './images/vonnegut.png',
+    alt: 'image of author kurt vonnegut jr.'
+  },
+
+];
